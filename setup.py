1a02a3c724925f7ee463ca9163f68f2e85223985
Piotr Toka <122290997+pntoka@users.noreply.github.com>
2024-10-27 17:05:47 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,20 @@ setup(
     name="derive_eq",
     version="0.1",
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'derive_eq=derive_eq.cli:main',
+        ],
+    },
     install_requires=['feedparser'],
     python_requires='>=3.11',
     author='Joe Bacchus, Alex, Piotr Toka',
     author_email='',
     description='Command line tool for getting derivation of equations',
     url='https://github.com/joebacchus/mathhack.git',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
